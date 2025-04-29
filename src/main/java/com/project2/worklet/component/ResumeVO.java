61e0c7c207e060eee70ab8437dc15ca7543c9253
sjl <sjl.tealblue@gmail.com>
2025-04-29 09:05:27 +0900

    이선진 0428 오후까지 작업량 0429 아침에 커밋

---
+++ src/main/java/com/project2/worklet/component/ResumeVO.java
@@ -0,0 +1,46 @@
+package com.project2.worklet.component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResumeVO {
+
+    private String username;
+    private String userBirthday;
+    private String userAddress;
+    private String userEmail;
+    private String userPhone;
+    private String army;
+    private String resumeEduPeriod1;
+    private String resumeEduPeriod2;
+    private String resumeEduCont1;
+    private String resumeEduCont2;
+    private String resumeCarPeriod1;
+    private String resumeCarPeriod2;
+    private String resumeCarBusiName1;
+    private String resumeCarBusiName2;
+    private String resumeCarWorkCont1;
+    private String resumeCarWorkCont2;
+    private String resumeCertRegDate1;
+    private String resumeCertRegDate2;
+    private String resumeCertRegDate3;
+    private String resumeCertRegDate4;
+    private String resumeCertName1;
+    private String resumeCertName2;
+    private String resumeCertName3;
+    private String resumeCertName4;
+    private String resumeCertFrom1;
+    private String resumeCertFrom2;
+    private String resumeCertFrom3;
+    private String resumeCertFrom4;
+    private String resumeEarlyLife;
+    private String resumeStrengthWeakness;
+    private String resumeApplyReason;
+    private String resumeApplyAfterDream;
+
+
+}
--- src/main/java/com/project2/worklet/controller/ResumeController.java
+++ src/main/java/com/project2/worklet/controller/ResumeController.java
@@ -1,24 +1,34 @@
 package com.project2.worklet.controller;
 
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfDocument;
+import com.lowagie.text.pdf.PdfWriter;
+import com.project2.worklet.component.ResumeVO;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
+
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class ResumeController {
+
+
+
 
     @PostMapping("/submitresume")
     public Map<String, String> airesume(@RequestBody Map<String, String> payload) {
@@ -43,7 +53,49 @@ public class ResumeController {
         return result;
     }
 
-//    @PostMapping("/generate-pdf")
+    @PostMapping("/generate-pdf")
+    public String generateStatus(@PathVariable("date") String date) throws Exception, JRException {
+
+        // jasper에 띄울 데이터 date에 맞춰 받아오기
+        List<ResumeVO> result = new ArrayList<>();
+        ResumeVO vo = new ResumeVO("이선진", "1990-09-18", "집주소",
+                "이메일@이메일.com", "010-1234-1234",
+                "기타", "2009.03-2013.02", "",
+                "한양대학교 분자생명과학부 졸업", "",
+                "2020.01-2024.07", "",
+                "성화물산 경영관리팀", "",
+                "무역사무", "","2024.11",
+                "TOEIC 920", "YBM토익", "",
+                "","","","","",
+                "","","",
+                "어린시절에는 ", "장단점은 ",
+                "지원동기는 ","포부는 ");
+        result.add(vo);
+
+        // 받아온 데이터를 jasper datasource로 등록
+        JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(result);
+
+        // jasper 컴파일할 양식 설정 - 만들어둔 jrxml 파일 경로 설정
+        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("src/main/resources/resume_ex_01.jrxml"));
+
+        // datasource를 매핑해 양식(jrxml)에 맞게 컴파일
+        HashMap<String, Object> map =new HashMap<>();
+        JasperPrint report = JasperFillManager.fillReport(compileReport, map, beanCollectionDataSource);
+
+        // return 방식1. 컴파일된 pdf파일을 현재 폴더에 생성
+			JasperExportManager.exportReportToPdfFile(report, "BoardStatus.pdf");
+			return "generated";
+
+        // return 방식2. 프린트 및 adobe pdf 화면 띄우기
+        // *주의: 프론트에서 화면을 띄울 수 없고, 서버 url을 직접 띄워야함..
+//        byte[] data = JasperExportManager.exportReportToPdf(report);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=BoardStatus.pdf");
+//
+//        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+    }
+
+    //    @PostMapping("/generate-pdf")
 //    public ResponseEntity<byte[]> generatePdf(@RequestBody Map<String, String> contents) throws Exception {
 //        // PDF를 저장할 메모리 스트림 생성
 //        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
